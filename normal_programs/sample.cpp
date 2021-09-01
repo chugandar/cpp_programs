31b2ff4a4e20670dc06afa33f9e090cee9a9677f
chugandar <furqanar03@gmail.com>
2021-09-01 12:39:41 +0530

    updated

--- normal_programs/sample.cpp
+++ normal_programs/sample.cpp
@@ -7,9 +7,12 @@ int main(){
     // cout<<typeid(s[0]).name();
     // cout<<__builtin_popcount(4)<<endl;
     // cout<<ceil(log2(9))<<endl;
-    int n=10;
-    vector<vector<int>> multi(n);
-    multi[1].push_back(1);
-    cout<<multi[1][0]<<endl;
+    // int n=10;
+    // vector<vector<int>> multi(n);
+    // multi[1].push_back(1);
+    // cout<<multi[1][0]<<endl;
+    string s = "Hello";
+    s.erase(s.begin() + s.length()-1);
+    cout<<s<<endl;
     return 0;
 }
